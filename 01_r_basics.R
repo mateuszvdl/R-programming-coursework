7679d1e0d53e340d6028ee8cbce7ac760715be14
mateuszvdl <147496678+mateuszvdl@users.noreply.github.com>
2025-07-13 15:24:38 +0200

    Initial commit of the first script

---
+++ 01_r_basics.R
@@ -0,0 +1,30 @@
+# Zajęcia 01
+
+# - To komentarz
+# Ctrl + L - czyści konsole
+
+5151+151
+1351-5445
+545*545
+3^2
+4546/454
+
+# Nasza pierwsza stała
+X <- 7
+
+X
+x # R jest case sensitive, lepiej pisać małymi literami
+
+# Pierwszy wektor
+wiek <- c(35, 44, 48)
+imiona <- c("Ben", "Max", "Kuba")
+
+ # wektor podstępny
+podstepny <- c("Max", 44)
+
+# funkcje w R
+getwd() # funkcja podająca katalog roboczy - ma zero argumentów
+em(x) #3 funkcja rm usuwa obiekty - ma jeden argument 
+
+# włączamy zainstalowany pakiet
+library(PogromcyDanych)
